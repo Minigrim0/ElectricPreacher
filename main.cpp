c7998264b80784b7134cc6a797dd5b5c41f2a8d6
fgrimau <florent.grimau@ulb.be>
2019-12-26 23:09:44 +0100

    [Add] Debug prints + tests for newly created console

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 #include "includes/screen.h"
 #include "includes/image_set.h"
 #include "includes/map_manager.h"
-
-#include "src/widgets.cpp"
+#include "includes/console.h"
 
 int main(){
+	if(DEBUG) std::cout << __PRETTY_FUNCTION__ << "> Creating screen" << std::endl;
 	Screen screen;
+	if(DEBUG) std::cout << __PRETTY_FUNCTION__ << "> Creating Image set" << std::endl;
 	ImageSet set;
 
-	Widget<std::string> chat;
 
 	screen.set_width(SCREEN_X);
 	screen.set_height(SCREEN_Y);
@@ -24,8 +24,14 @@ int main(){
 	set.set_image(&screen, "assets/images/Doors.png");
 	set.set_array();
 
+	if(DEBUG) std::cout << __PRETTY_FUNCTION__ << "> Creating console" << std::endl;
+	Console cons(screen.get_font());
+	if(DEBUG) std::cout << __PRETTY_FUNCTION__ << "> Updating layout" << std::endl;
+	cons.update_layout(&screen);
+
 	while(screen.is_running()){
 		screen.handle_events();
+
 		screen.blit(set.get_texture(), set.get_sub(0, 9),  0, 10);
 		screen.blit(set.get_texture(), set.get_sub(0, 10), 0, 42);
 		screen.blit(set.get_texture(), set.get_sub(0, 11), 0, 74);
@@ -37,6 +43,9 @@ int main(){
 		screen.blit(set.get_texture(), set.get_sub(2, 9),  64, 10);
 		screen.blit(set.get_texture(), set.get_sub(2, 10), 64, 42);
 		screen.blit(set.get_texture(), set.get_sub(2, 11), 64, 74);
+
+		cons.draw(&screen);
+
 		screen.update_screen();
 	}
 
