885184685b2b58c53936db15caac956d27d18fcf
fgrimau <florent.grimau@ulb.be>
2020-01-13 21:11:10 +0100

    [Add] member initialization list

--- src/notification.cpp
+++ src/notification.cpp
@@ -1,10 +1,24 @@
 #include "../includes/notification.h"
 
 //Constructors
-Notification::Notification(){
+Notification::Notification()
+:m_content(""),
+m_background(nullptr),
+m_text_texture(nullptr),
+m_icon(nullptr)
+{
 
 }
 
 Notification::~Notification(){
 
+}
+
+
+int Notification::draw(Screen*){
+    return 0;
+}
+
+int Notification::update(SDL_Event*, Screen*){
+    return 0;
 }
