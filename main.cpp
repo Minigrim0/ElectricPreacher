ab8ea793806f8acdc2e5375f231996c543963412
Minigrim0 <florent.grimau@outlook.com>
2020-06-30 17:27:36 +0200

    [Upd] Main + add MainMenu json

--- main.cpp
+++ main.cpp
@@ -10,14 +10,15 @@
 #include "includes/window.h"
 
 int main(){
+	std::cout << "Creating screen object" << std::endl;
 	Screen screen;
-	ImageSet set;
-
-	Widget** widgets = static_cast<Widget**>(malloc(sizeof(Widget*)*5));
+	std::cout << "Creating window object" << std::endl;
+	Window window;
 
 	SDL_Event* event_handler = new SDL_Event;
 	SDL_StopTextInput();
 
+	std::cout << "Creating screen" << std::endl;
 	screen.set_width(SCREEN_X);
 	screen.set_height(SCREEN_Y);
 	screen.set_caption(
@@ -26,43 +27,27 @@ int main(){
 		std::to_string(SUBVERSION) + "." +
 		std::to_string(SUBSUBVERSION) + ")");
 
+	std::cout << "Initializing screen" << std::endl;
 	screen.init();
 	if(screen.build_window() != 0) return EXIT_FAILURE;
 
-	set.set_image(&screen, "assets/images/Doors.png");
-	set.set_array();
+	std::cout << "Loading window's fonts" << std::endl;
+	window.set_font("assets/fonts/Roboto-Regular.ttf");
 
-	//Console* cons = new Console(10, 150, 250, 400);
-	//cons->set_font("assets/fonts/Roboto-Regular.ttf", 20);
-	//cons->init(&screen);
-	//cons->update_layout(&screen);
-	//widgets[0] = cons;
+	std::cout << "Creating main window" << std::endl;
+	window.createfrom(&screen, "assets/UI/setup.json");
 
 	while(screen.is_running()){
 
-		screen.blit(set.get_texture(), set.get_sub(0, 9),  0, 10);
-		screen.blit(set.get_texture(), set.get_sub(0, 10), 0, 42);
-		screen.blit(set.get_texture(), set.get_sub(0, 11), 0, 74);
-
-		screen.blit(set.get_texture(), set.get_sub(3, 9),  32, 10);
-		screen.blit(set.get_texture(), set.get_sub(3, 10), 32, 42);
-		screen.blit(set.get_texture(), set.get_sub(3, 11), 32, 74);
-
-		screen.blit(set.get_texture(), set.get_sub(2, 9),  64, 10);
-		screen.blit(set.get_texture(), set.get_sub(2, 10), 64, 42);
-		screen.blit(set.get_texture(), set.get_sub(2, 11), 64, 74);
-
-		//widgets[0]->draw(&screen);
-
+		window.draw(&screen);
 
 	    while(SDL_PollEvent(event_handler) != 0){
 			screen.handle_events(event_handler);
-			//widgets[0]->update(event_handler, &screen);
+			window.update(event_handler, &screen);
 		}
 
 		screen.update_screen();
 	}
 
-	free(widgets);
 	return EXIT_SUCCESS;
 }
