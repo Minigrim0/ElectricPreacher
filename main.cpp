2194c710fbba72ff1a79074d0012a4452e67c394
Minigrim0 <florent.grimau@outlook.com>
2020-07-24 19:16:25 +0200

    [Upd] windows are linked

--- main.cpp
+++ main.cpp
@@ -47,11 +47,12 @@ int main(){
     for (const auto& entry : std::filesystem::directory_iterator(pathToShow)) {
         const auto filenameStr = entry.path().filename().string();
         if(entry.is_regular_file()){
-            std::cout << "Found window " << filenameStr << std::endl;;
+            std::cout << "Found window " << filenameStr << std::endl;
             Window tmp_window;
             tmp_window.set_font("assets/fonts/Roboto-Regular.ttf");
-            tmp_window.createfrom(&screen, "assets/UI/MainWindow.json");
+            tmp_window.createfrom(&screen, static_cast<std::string>(pathToShow) + static_cast<std::string>(filenameStr));
 
+            std::cout << "Saving window as " << tmp_window.get_title() << std::endl;
             windows[tmp_window.get_title()] = tmp_window;
         }
     }
