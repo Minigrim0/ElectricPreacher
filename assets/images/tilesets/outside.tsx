878f61e0510fa3877dece7f13c8bb616a3eae703
Minigrim0 <grimauflorent@gmail.com>
2021-08-13 20:34:50 +0200

    [Add] map test

---
+++ assets/images/tilesets/outside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Outside" tilewidth="32" tileheight="32" tilecount="180" columns="20">
+ <image source="../tiles/IceTileset.png" width="640" height="288"/>
+</tileset>
