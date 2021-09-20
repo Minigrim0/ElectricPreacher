d8ac40f533849d9cb38b244dd91807fe97213d3c
Minigrim0 <grimauflorent@gmail.com>
2021-09-20 08:48:23 +0200

    [Upd] moved files in their folders

--- includes/utils.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef utils_hpp
-    #define utils_hpp
-
-#include <SDL2/SDL.h>
-#include "../src/screen/includes/screen.hpp"
-
-void save_texture(Screen* screen, SDL_Texture* texture, const char* filename);
-
-#endif
