181c0f703c3f0b693f990f8ddd0324513c60ce6a
Minigrim0 <grimauflorent@gmail.com>
2023-05-12 00:05:35 +0200

    [Fix] changed icons to a tileset

---
+++ assets/images/tilesets/icons.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="icons" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="../tiles/icons.png" width="196" height="64"/>
+</tileset>
