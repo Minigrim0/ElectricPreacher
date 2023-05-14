4c8d47db48252c6968e49fa0a536547b66507e5b
Minigrim0 <grimauflorent@gmail.com>
2023-05-14 19:32:34 +0200

    [Upd] Assets paths

---
+++ assets/maps/tilesets/outside.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Outside" tilewidth="32" tileheight="32" tilecount="180" columns="20">
+ <image source="../tiles/IceTileset.png" width="640" height="288"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
