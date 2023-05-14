e761c90896867ae8b986b83953edcd87d461e7da
Minigrim0 <grimauflorent@gmail.com>
2023-05-14 18:53:19 +0200

    [Upd] Constants

--- constants.hpp
+++ constants.hpp
@@ -1,29 +1,11 @@
 #pragma once
 
-//Map
-#define CHUNK_SIZE 16
+// Project
+#define PROJECT_NAME "Electric Preacher"
 
 //Screen
 #define SCREEN_X 1920
 #define SCREEN_Y 1080
-
-//Directions
-enum Directions
-{
-    DIRECTION_UP,
-    DIRECTION_DOWN,
-    DIRECTION_LEFT,
-    DIRECTION_RIGHT,
-};
-
-//SceneryElement
-enum ElementType
-{
-    TYPE_SIGN,
-    TYPE_LEVER,
-    TYPE_TABLE,
-    TYPE_ITEM,
-};
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
