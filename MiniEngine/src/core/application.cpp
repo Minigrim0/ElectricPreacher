4f81fef3358cb64dc477a03b72447ad62ea6d263
Minigrim0 <grimauflorent@gmail.com>
2023-05-19 09:57:56 +0200

    [Add] version constructor in application

--- MiniEngine/src/core/application.cpp
+++ MiniEngine/src/core/application.cpp
@@ -4,8 +4,29 @@
 
 namespace MiniEngine {
     Application::Application():m_project_name("MiniEngine Application") {}
-    Application::Application(std::string project_name):m_project_name(project_name) {
+
+    Application::Application(std::string project_name)
+    :m_project_name(project_name),
+    m_major_version(0),
+    m_minor_version(1),
+    m_patch_version(0)
+    {
         ME_CORE_INFO("Application {0} created!", project_name);
+    }
+
+    Application::Application(std::string project_name, int major_version, int minor_version, int patch_version)
+    :m_project_name(project_name),
+    m_major_version(major_version),
+    m_minor_version(minor_version),
+    m_patch_version(patch_version)
+    {
+        ME_CORE_INFO(
+            "Application {0} ({1}.{2}.{3}) created!",
+            project_name,
+            major_version,
+            minor_version,
+            patch_version
+            );
     }
 
     Application::~Application(){}
@@ -18,10 +39,10 @@ namespace MiniEngine {
         m_screen->set_width(1920);
         m_screen->set_height(1080);
         m_screen->set_caption(
-            "The Electric Preacher (" +
+            m_project_name +
             std::to_string(0) + "." +
             std::to_string(1) + "." +
-            std::to_string(2) + ")");
+            std::to_string(2));
         m_screen->init();
         m_screen->add_font("assets/fonts/Roboto-Regular.ttf", 16, "Roboto_16");
         m_screen->set_default_font("Roboto_16");
@@ -32,10 +53,15 @@ namespace MiniEngine {
         }
         ME_CORE_INFO("Started application");
 
-        while (m_running) {
-            
-            // Handle events
+        SDL_Event event;
 
+        while (m_running) {
+            // Handle events
+            while(SDL_PollEvent(&event)){
+                if (event.type == SDL_QUIT) {
+                    m_running = false;
+                }
+            }
             m_screen->update_screen();
         }
     }
--- MiniEngine/src/core/application.hpp
+++ MiniEngine/src/core/application.hpp
@@ -13,12 +13,17 @@ namespace MiniEngine {
     public:
         Application();
         Application(std::string project_name);
+        Application(std::string project_name, int major_version, int minor_version, int patch_version);
         virtual ~Application();
 
         void run();
 
     protected:
         std::string m_project_name;
+        int m_major_version;
+        int m_minor_version;
+        int m_patch_version;
+
         std::unique_ptr<Screen> m_screen;
         std::unique_ptr<UI::NotificationCenter> m_notification_center;
         std::unique_ptr<Event::LayerManager> m_layer_manager;
