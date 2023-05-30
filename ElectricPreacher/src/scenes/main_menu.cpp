3768add750a4d7dfa818cb65f21914fb7496bba1
Minigrim0 <grimauflorent@gmail.com>
2023-05-30 14:25:39 +0200

    [Upd] added button on the main menu

--- ElectricPreacher/src/scenes/main_menu.cpp
+++ ElectricPreacher/src/scenes/main_menu.cpp
@@ -1,36 +1,74 @@
 #include "scenes/main_menu.hpp"
 
+#include <core/log.hpp>
 #include <UI/widgets/button.hpp>
 
-#include <core/log.hpp>
 
 MainMenu::MainMenu(MiniEngine::Application* app) : Scene(app, "MainMenu") {
-    // ME_INFO("Creating main menu");
+    ME_INFO("Creating main menu");
 }
 
-void callback(){
-    // ME_INFO("Button pressed");
-}
-
-void add_button(MiniEngine::Scene* scene, int posx){
-    MiniEngine::UI::Widgets::Button* button = new MiniEngine::UI::Widgets::Button();
-    button->set_text("Hello World");
-    button->set_position(100 + posx * 5, 100 + posx * 5);
-    button->set_size(200, 50);
-    button->set_background_color(255, 0, 0);
-    button->set_text_color(0, 0, 0); 
-    button->update_layout();
-
-    button->set_callback(callback);
-    scene->add_interactible(posx, button);
-}
 
 MainMenu::~MainMenu(){
     ME_INFO("Destroying main menu");
 }
 
+void MainMenu::start_new_game(){
+    ME_INFO("starting new game");
+}
+
+void MainMenu::load_game(){
+    ME_INFO("loading game");
+}
+
+void MainMenu::quit_game(){
+    ME_INFO("quitting game");
+}
+
 void MainMenu::init(MiniEngine::Application* app){
     ME_INFO("Initializing main menu");
-    for(auto x = 0; x < 10; x++)
-        add_button(m_scene, x);
+
+    MiniEngine::UI::Widgets::Button* title_button = new MiniEngine::UI::Widgets::Button();
+    MiniEngine::UI::Widgets::Button* new_button = new MiniEngine::UI::Widgets::Button();
+    MiniEngine::UI::Widgets::Button* load_button = new MiniEngine::UI::Widgets::Button();
+    MiniEngine::UI::Widgets::Button* quit_button = new MiniEngine::UI::Widgets::Button();
+
+    title_button->set_size(200, 50);
+    title_button->set_text("The Electric Preacher");
+    title_button->set_position(860, 100);
+    title_button->set_background_color(0, 0, 0, 0);
+    title_button->set_text_color(200, 200, 200); 
+    title_button->update_layout();
+
+    new_button->set_size(200, 25);
+    new_button->set_text("New Game");
+    new_button->set_position(860, 485);
+    new_button->set_background_color(20, 50, 20);
+    new_button->set_text_color(150, 150, 150);
+    new_button->set_contour_color(50, 150, 50);
+    new_button->set_callback(std::bind(&MainMenu::start_new_game, this));
+    new_button->update_layout();
+
+    load_button->set_size(200, 25);
+    load_button->set_text("Load Game");
+    load_button->set_position(860, 530);
+    load_button->set_text_color(150, 150, 150);
+    load_button->set_background_color(20, 20, 50);
+    load_button->set_contour_color(50, 50, 150);
+    load_button->set_callback(std::bind(&MainMenu::load_game, this));
+    load_button->update_layout();
+
+    quit_button->set_size(200, 25);
+    quit_button->set_text("Quit");
+    quit_button->set_position(860, 575);
+    quit_button->set_text_color(150, 150, 150);
+    quit_button->set_background_color(50, 20, 20);
+    quit_button->set_contour_color(150, 50, 50);
+    quit_button->set_callback(std::bind(&MainMenu::quit_game, this));
+    quit_button->update_layout();
+
+    m_scene->add_interactible(0, title_button);
+    m_scene->add_interactible(1, new_button);
+    m_scene->add_interactible(1, load_button);
+    m_scene->add_interactible(1, quit_button);
 }
--- ElectricPreacher/src/scenes/main_menu.hpp
+++ ElectricPreacher/src/scenes/main_menu.hpp
@@ -11,5 +11,9 @@ class MainMenu : public Scene {
         void init(MiniEngine::Application* app) override;
 
     private:
+        void start_new_game();
+        void load_game();
+        void quit_game();
+
         MiniEngine::Scene* scene;
 };
