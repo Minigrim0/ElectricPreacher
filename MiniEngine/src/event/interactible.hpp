a5e602d529adbe747a4268a142fe2b9c2037bc71
Minigrim0 <grimauflorent@gmail.com>
2024-05-13 15:00:48 +0200

    only force OnEvent on interactibles

--- MiniEngine/src/event/interactible.hpp
+++ MiniEngine/src/event/interactible.hpp
@@ -5,16 +5,12 @@
 #include "core/core.hpp"
 #include "core/screen.hpp"
 
-namespace MiniEngine {
-namespace Event {
+namespace MiniEngine::Event {
 class ME_API Interactible {
   public:
     Interactible() = default;
     virtual ~Interactible() = default;
 
     virtual bool OnEvent(SDL_Event *event) = 0;
-    virtual void OnRender(Screen *sc) = 0;
-    virtual void OnUpdate(int time_elapsed) = 0;
 };
 } // namespace Event
-} // namespace MiniEngine
